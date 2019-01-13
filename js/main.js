b99a78604965fed28324f3482c0841cba1799c46
Grzegorz Nęcka <strzypior@gmail.com>
2019-01-13 16:22:08 +0100

    pierwszy commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: "#app",
+    data: {
+
+        colors: [
+            {name: "niebieski", value: "blue"},
+            {name: "zielony", value: "green"},
+            {name: "czerwony", value: "red"},
+            {name: "fioletowy", value: "purple"},
+            {name: "pomarańczowy", value: "orange"},
+        ],
+        color: ""
+    }
+
+});
